c7cd1ca14ab278357ff7c081ef7f452a73eb16f1
Queengeorge <queenkingsleygeorge@gmail.com>
2024-10-31 14:39:41 +0100

    new message

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+
+
+mysql -u username -p alx_book_store < task_5.sql
